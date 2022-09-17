4c197fb288aeaeae43729c906130898953a67f7e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-17 11:29:45 +0600

    navigation bar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -6,11 +6,16 @@ const Navbar = () => {
         <div className='navContainer'>
           <div className='navWrapper'>
             <div className='nlogo'>
-
+              <h2>logo</h2>
             </div>
             <div className='nItem'>
-                
+               <div>Home</div>
+               <div>Our Portfolio</div>
+               <div>Our team</div>
+               <div>Contact Us</div>
+               <button className='lButton'>Login</button>
             </div>
+            
           </div>
         </div>
     );
