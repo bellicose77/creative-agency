c5792c01090d316d91f1b8b185ce189d531738c0
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-17 17:07:38 +0600

    save user in the database

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,8 +2,11 @@ import User from "../models/User.js"
 import bcrypt from 'bcrypt'
 
 export const register = async(req,res,next) =>{
+    //const saltRounds = 10;
     try{
-     const newUser = new User({
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        const newUser = new User({
         username:req.body.username,
         password:req.hash,
         email:req.body.email
