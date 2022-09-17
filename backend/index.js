77f96e7c4b22be4d57a40718a3f06a563db9f302
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-17 13:13:34 +0600

    mongodb connections

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import mongoose  from 'mongoose';
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config();
 const port = 8000;
+const connect = async() =>{
+    try{
+
+    }catch(err){
+        throw err;
+    }
+}
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
