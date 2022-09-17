79b4ab03461dd10f1ecfcf99961e83d03381f456
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-17 17:04:42 +0600

    save user in the database

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,14 @@
+import User from "../models/User.js"
+import bcrypt from 'bcrypt'
+
 export const register = async(req,res,next) =>{
     try{
-
+     const newUser = new User({
+        username:req.body.username,
+        password:req.hash,
+        email:req.body.email
+     });
+     await newUser.save();
     }catch(err){
 
     }
