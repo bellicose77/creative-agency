1327a13f3e5a9c34c9cb914233028d0a7c3dd27d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-17 13:25:41 +0600

    add all route and model and controllers

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,22 @@ import dotenv from 'dotenv'
 const app = express();
 dotenv.config();
 const port = 8000;
+
+// database connection and query
 const connect = async() =>{
     try{
         await mongoose.connect(process.env.MONGO);
-        console.log("connected to the mongodb")
+        console.log("connected to the mongodb");
     }catch(err){
         throw err;
     }
-}
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+};
+
+// all middel ware will be here
+app.use(express.json())
+
   
   app.listen(port, () => {
     connect()
     console.log(`Example app listening on port ${port}`)
-  })
+  });
