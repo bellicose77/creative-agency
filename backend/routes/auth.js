e67ab9311f542db2d3ac7f6ec6afbff3eee9fd3a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-17 14:19:50 +0600

    user model

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,8 @@
 import express  from "express"
+import { register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.post('/register');
+router.post('/register',register);
 
 export default router;
