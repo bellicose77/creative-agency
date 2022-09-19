07a6b6ea494c81adf21cca0ee9c93ed5490f8de8
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-19 13:15:29 +0600

    register form design

--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -3,7 +3,29 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-           <h2>This is register</h2> 
+           <div className='regContainer'>
+            <div className='regWrapper'>
+                <div className='regHeader'>
+                 <h2>Register here</h2>
+                </div>
+                <div className='regForm'>
+                    <form>
+                        <lable>Name: </lable>
+                        <input type="text"/>
+                        <lable>Email: </lable>
+                        <input type="email"/>
+                        <lable>Password: </lable>
+                        <input type="password"/>
+                        <lable>Admin: </lable>
+                        <input type="Boolean"/>
+                         
+                    </form>
+                    <button>Submit</button>
+                </div>
+
+            </div>
+
+           </div>
         </div>
     );
 };
