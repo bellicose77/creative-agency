b84c6bd3686a4a060109569c76d8d4c6db0687d4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-21 11:53:36 +0600

    Service Schema and model

--- backend/models/Service.js
+++ backend/models/Service.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
 const serviceSchema = new mongoose.Schema({
-    
-})
+    title:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    }
+
+},
+{timestamps: true}
+)
