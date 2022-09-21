8d7cdfc41e8ce119cf0426b48dee5ffd21ff151c
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-21 12:49:30 +0600

    footer container

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footerContainer'>
+            <div className='footerWrapper'>
+                <div className='footerTextContainer'>
+
+                </div>
+                <div className='footerFormContainer'>
+                    
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Footer;
