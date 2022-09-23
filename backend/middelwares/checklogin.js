1300c2ef4de8b6f461ab221ff3ed9da1faf1edb4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-23 13:44:56 +0600

    authorization done

--- backend/middelwares/checklogin.js
+++ backend/middelwares/checklogin.js
@@ -1,3 +1,13 @@
-export const checkLoging = async(req,res,next)=>{
-    
-}
+import jwt from 'jsonwebtoken'
+const checkLoging = async(req,res,next)=>{
+    const {authorization} = req.headers;
+    console.log(authorization)
+    try{
+        const token = authorization.split(' ')[1];
+        const decode = jwt.verify(token,process.env.JWT_SECRET)
+        next();
+    }catch(err){
+       res.status(400).json("token failed");
+    }
+};
+export default checkLoging;
--- backend/routes/service.js
+++ backend/routes/service.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { addService, deleteService, getServices, getSingleService, updateService } from '../controllers/service.js';
+import verifyUser from '../middelwares/checklogin.js'
 
 const router = express.Router();
 
 // add a new service
 router.post('/services',addService);
 //get all the service
-router.get('/services',getServices);
+router.get('/services',verifyUser,getServices);
 //get a single service 
 router.get('/services/:id',getSingleService);
 // delete a service
