da9e8ffb230c6514d6122670f2b5314b357efab4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-25 16:30:31 +0600

    fetching data from data base

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
+import Services from '../../components/Services/Services';
 
 const Home = () => {
     return (
         <div>
           <Navbar/> 
           <Header/> 
+          <Services/>
           <Footer/>
         </div>
     );
