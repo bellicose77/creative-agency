4f4093249f9e8fba3e6a1d5499903192c355d9bc
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-27 16:08:04 +0600

    service detail

--- frontend/src/pages/ServiceDetail/ServiceDetail.js
+++ frontend/src/pages/ServiceDetail/ServiceDetail.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './ServiceDetail.css'
 
 const ServiceDetail = () => {
     return (
         <div>
-            
+            <h2>Service detail is here</h2>
         </div>
     );
 };
